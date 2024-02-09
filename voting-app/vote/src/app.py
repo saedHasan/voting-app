46619b7cf49461d1ae410a7b6db5e73aa975b0db
saedHasan <saed.it.cis@gmail.com>
2024-02-10 02:39:21 +0300

    Update app.py
    new

--- voting-app/vote/src/app.py
+++ voting-app/vote/src/app.py
@@ -9,7 +9,6 @@ import logging
 option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
-redis_password=os.getenv('REDIS_PASSWORD' , "Dogs")
 
 app = Flask(__name__)
 
@@ -19,7 +18,7 @@ app.logger.setLevel(logging.INFO)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5, password='redis_password')
+        g.redis = Redis(host="redis", db=0, socket_timeout=5)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
@@ -49,4 +48,4 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
