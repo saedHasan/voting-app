4a9032c92df4b1e9b1d2d725aa5a7f400a4735a4
saedHasan <saed.it.cis@gmail.com>
2024-02-11 22:03:48 +0300

    Update server.js

--- voting-app/result/src/server.js
+++ voting-app/result/src/server.js
@@ -1,14 +1,20 @@
 var express = require('express'),
     async = require('async'),
+    pg = require('pg'),
     { Pool } = require('pg'),
+    path = require('path'),
     cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
     app = express(),
     server = require('http').Server(app),
     io = require('socket.io')(server);
 
+io.set('transports', ['polling']);
+
 var port = process.env.PORT || 8080;
 
-io.on('connection', function (socket) {
+io.sockets.on('connection', function (socket) {
 
   socket.emit('message', { text : 'Welcome!' });
 
@@ -17,7 +23,7 @@ io.on('connection', function (socket) {
   });
 });
 
-var pool = new Pool({
+var pool = new pg.Pool({
   connectionString: 'postgres://postgres:postgres@db/postgres'
 });
 
@@ -64,7 +70,15 @@ function collectVotesFromResult(result) {
 }
 
 app.use(cookieParser());
-app.use(express.urlencoded());
+app.use(bodyParser());
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
+
 app.use(express.static(__dirname + '/views'));
 
 app.get('/', function (req, res) {
