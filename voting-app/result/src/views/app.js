2ef89a17de523d24ae611439ece69736adb6597b
saedHasan <saed.it.cis@gmail.com>
2024-02-11 23:17:08 +0300

    Update app.js

--- voting-app/result/src/views/app.js
+++ voting-app/result/src/views/app.js
@@ -5,7 +5,7 @@ var bg1 = document.getElementById('background-stats-1');
 var bg2 = document.getElementById('background-stats-2');
 
 app.controller('statsCtrl', function($scope){
-  $scope.aPercent = 50;
+  $scope.aPercent = 0;
   $scope.bPercent = 50;
 
   var updateScores = function(){
