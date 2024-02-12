0712bd27ef640e5cd12063c75d71eda09f511a55
saedHasan <saed.it.cis@gmail.com>
2024-02-12 15:42:56 +0300

    Update app.js

--- voting-app/result/src/views/app.js
+++ voting-app/result/src/views/app.js
@@ -1,5 +1,5 @@
 var app = angular.module('catsvsdogs', []);
-var socket = io.connect({transports:['polling']});
+var socket = io.connect();
 
 var bg1 = document.getElementById('background-stats-1');
 var bg2 = document.getElementById('background-stats-2');
@@ -10,12 +10,10 @@ app.controller('statsCtrl', function($scope){
 
   var updateScores = function(){
     socket.on('scores', function (json) {
-       console.log('updateScores im in '); // Log received data
-
        data = JSON.parse(json);
        var a = parseInt(data.a || 0);
        var b = parseInt(data.b || 0);
-       console.log('Received scoressssssssssss:', data); // Log received data
+
        var percentages = getPercentages(a, b);
 
        bg1.style.width = percentages.a + "%";
@@ -28,35 +26,13 @@ app.controller('statsCtrl', function($scope){
        });
     });
   };
- // Log when the connection is established
-socket.on('connect', function() {
-    console.log('Connected to Socket.IO server');
-});
-
-// Log when data is received
-socket.on('scores', function(json) {
-    console.log('Received scores:', json);
-});
-
-// Log any errors
-socket.on('error', function(error) {
-    console.error('Socket.IO error:', error);
-});
-
-// Log when the connection is closed
-socket.on('disconnect', function() {
-    console.log('Disconnected from Socket.IO server');
-});
 
   var init = function(){
-    console.log('im in init');
     document.body.style.opacity=1;
-    console.log('init 2');
     updateScores();
   };
   socket.on('message',function(data){
     init();
-    updateScores();
   });
 });
 
