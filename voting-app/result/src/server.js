050f8d68be384e98364839c10a68e1e6a8038756
saedHasan <saed.it.cis@gmail.com>
2024-02-12 14:19:27 +0300

    Update server.js

--- voting-app/result/src/server.js
+++ voting-app/result/src/server.js
@@ -31,7 +31,7 @@ io.sockets.on('connection', function (socket) {
 });
 
 var pool = new pg.Pool({
-  connectionString: 'postgres://postgres:postgres@db/postgres'
+  connectionString: 'postgres://postgres:postgres@db/postgressssssss'
 });
 
 async.retry(
